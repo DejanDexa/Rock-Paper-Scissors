63ed1a7783fa6afc27f887626b4f9a244310ec31
DejanDexa <dexapage@gmail.com>
2020-05-13 18:20:16 +0200

    Redesing and styling

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ const game = () => {
         const options = document.querySelectorAll(".options button");
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll(".hands img");
 
+        //Animation start-end
+        hands.forEach(hand => {
+            hand.addEventListener("animationend", function(){
+                this.style.animation = "";
+            });
+        });
 
         //Computer options
         const computerOptions = ["rock", "paper", "scissors"];
@@ -30,14 +37,16 @@ const game = () => {
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions [computerNumber];
 
+                setTimeout(() => {
                 //Call compare hands
                 compareHands(this.textContent, computerChoice);
-
-
                 //Update images
                 playerHand.src = `./img/${this.textContent}.png`;
                 computerHand.src = `./img/${computerChoice}.png`;
-
+                }, 2000);
+                //Animation
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
             });
         });
     };
